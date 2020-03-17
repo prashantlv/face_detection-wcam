c9f51b00cd63e42ce950d84202237d9f75339925
Prashant Shukla <shukla.prashant689@gmail.com>
2020-03-18 00:21:51 +0530

    Update face_app.py

--- face_app.py
+++ face_app.py
@@ -29,7 +29,7 @@ while True:
         minSize=(30, 30)
     )
 
-    # Draw a rectangle around the faces
+    # Draw a rectangle
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
@@ -38,16 +38,13 @@ while True:
         log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
 
 
-    # Display the resulting frame
     cv2.imshow('Video', frame)
 
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    # Display the resulting frame
     cv2.imshow('Video', frame)
 
-# When everything is done, release the capture
 video_capture.release()
 cv2.destroyAllWindows()
